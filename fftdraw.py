85b5b51f4fc61060136fb0e8a7dfeac2ab67afeb
full-elevator <amailinuse@163.com>
2023-07-15 19:32:57 +0800

    Split parameter reading to a separate file

--- fftdraw.py
+++ fftdraw.py
@@ -4,90 +4,33 @@ from pydub import AudioSegment
 from pydub.playback import play
 from pydub.effects import normalize
 import numpy as np
-from array import array #pydub has issues with float32 or int64, see issue #293
+#pydub works with array but has issues with np.int32 or np.int64, see issue #293
+from array import array
 import warnings
+from fftinput import *
 
-help_text = """
-Input the parameters in the following order, separated by a space:
- right limit of x axis (default 800);
- n of sampling points in a single segment (default 4410);
- applied effects: f to fade out and/or n to normalize (default n);
- number of sampling points to plot: not plotted if 0 (default 0);
- export path: if omitted, no file is exported (default None).
-
-When the plot window is focused:
-"1" (the number) to add a solitary peak;
-Hold down "1" and move the cursor to draw a line;
-"r" or "(" to smoothen the real part curve;
-"i" or ")" to smoothen the imaginary part curve;
-and "Enter" to hear the sound.
-
-The smooth curve is calculated based on the black X waypoints.
-
-Try setting "800 800" for the parameters to remove jitter.
-"""
-print("FFTdraw: draw a spectrum and hear how it goes. Type help for help.")
-def input_params():
-    """Take a string to set parameters for the sound."""
-    param_string = input("Parameters > ").lower()
-    defaults = [800, 4410, "n", 0, None]
-    params = param_string.split(" ")
-    #remove the empty string
-    if not params[0]:
-        params.pop(0)
-    try:
-        while len(params)!=5:
-            params.append(defaults[len(params)])
-        
-        if params[0]=="help":
-            print(help_text)
-            return(input_params())
-        
-        lim_x = int(params[0])
-        fft_length = int(params[1])
-        effects = params[2]
-        plot_domain = int(params[3])
-        out_path = params[4]
-        
-        if lim_x>fft_length:
-            warnings.warn(f"""The right limit {lim_x} is greater than
-                the FFT length {fft_length}. Setting to {fft_length}""")
-            lim_x = fft_length
-            
-        if out_path: #specify the format if missing
-            if out_path[-4]!=".":
-                out_path += ".wav"
-                
-        print("The parameters are", lim_x, fft_length, effects, plot_domain, out_path)
-        return(lim_x, fft_length, effects, plot_domain, out_path)
-    except:
-        warnings.warn(f"""Invalid numberical values, or too many values to unpack.
-            Using default values {defaults}.""")
-        return(defaults)
-
-def get_swipe_width(lim_x):
-    """Determine how many adjacent values to set as event.ydata to prevent jags."""
-    if 0<lim_x<=90: width = 0
-    elif 90<lim_x<=200: width = 1
-    elif 200<lim_x<=300: width = 2
-    elif 300<lim_x<=400: width = 3
-    elif 400<lim_x<=800: width = 4
-    else: width = 5
-    return(width)
+def ms2sample(ms):
+    return(ms/1000*rate)
 
 class LineSegment:
     """Define a line segment in the plotting area."""
-    def __init__(self, ax):
-        self.ax = ax    #"ax1" for real, "ax2" for imag
-        self.X = np.arange(0, lim_x, dtype=int)
-        self.Y = np.zeros((lim_x,), dtype=float)
+    def __init__(self, ax, Y_in=None):
+        #The real line is in ax1, the imaginary line is in ax2.
+        self.ax = ax   
         self.vertices = {0:0.0, lim_x:0.0}
-        #self.line = Line #matplotlib line2D object
+        self.X = np.arange(0, lim_x, dtype=int)
+
+        if type(Y_in) is np.ndarray:
+            self.Y = Y_in
+        else:
+            self.Y = np.zeros((lim_x,), dtype=float)
+
+        self.edges, = ax.plot([], [], "xk") #black X markers            
         if ax==ax1:
-            self.line, = ax.plot([], [], "b")
+            self.line, = ax.plot(self.X, self.Y, "b")
         elif ax==ax2:
-            self.line, = ax.plot([], [], "C1")
-        self.edges, = ax.plot([], [], "xk") #black X markers
+            self.line, = ax.plot(self.X, self.Y, "C1")
+        
         
     def draw_peak(self, x, y):
         if x<0 or x>=lim_x:
@@ -101,31 +44,20 @@ class LineSegment:
             #print(self.vertices)
 
     def draw_swipe(self, x, y):
-        if not x or not y: return
-        if 0<=x<=lim_x:
-            for i in range(max(0,x-swipe_w), min(x+swipe_w,lim_x)):
-                self.Y[i] = y
+        if not x or not y or x<0 or x>lim_x:
+            return
+        
+        #print(max(0,x-params["swipe"]), min(params["swipe"],lim_x))
+        for i in range(max(0,x-params["swipe"]), min(x+params["swipe"],lim_x)):
+            self.Y[i] = y
 
-            self.line.set_data(self.X, self.Y)
+        self.line.set_data(self.X, self.Y)
                 
     def make_smooth_curve(self):
         vertices_X, vertices_Y = bezier_smooth(self.vertices)
-        #print("Vertices generated successfully.")
         self.Y = np.interp(self.X, vertices_X, vertices_Y)
         self.line.set_data(self.X, self.Y)
 
-    def scale_data(self, resolution):
-        """Scale the user input to calibrate to the desired frequency."""
-        i = 0.0
-        values_rar = array("h", [])
-        while i<len(self.Y):
-            try:
-                values_clip = self.Y[int(i):int(i+resolution)]
-            except IndexError:
-                values_clip = self.Y[int(i):]
-            values_rar.append(int(max(values_clip))) #max() preserves the peak
-            i += resolution
-        return(values_rar)
 
 def detect_keyboard(event):
     """Detect key presses."""
@@ -136,16 +68,16 @@ def detect_keyboard(event):
     elif key==")" or key=="i":
         Imag.make_smooth_curve()
     elif key=="enter":
-        reals = Real.scale_data(44100 / fft_length)
-        imags = Imag.scale_data(44100 / fft_length)
+        reals = Real.Y
+        imags = Imag.Y
         if sum(imags)==0:
             warnings.warn("Imaginary part is empty, default to equate real part")
             imags = reals
+            
         samples_fft = np.zeros((fft_length,), dtype=complex)
-        vertical_scale = 60000 / lim_x     #prevent overflow
         for i in range(len(reals)):
-            samples_fft[i] = complex(vertical_scale*reals[i],
-                                     vertical_scale*imags[i])
+            samples_fft[i] = complex(y_scale*reals[i],
+                                     y_scale*imags[i])
             
         plt.close()
         convert_to_sound(samples_fft, fft_length)
@@ -156,14 +88,13 @@ def detect_keyboard(event):
     
     if key=="1":
         lines[ax].draw_peak(int(event.xdata), event.ydata)
-        #lines("vertices_"+ax).set_data(.keys(), D.values())     
     
 def detect_swipe(event):
     """Detect cursor swipe motion."""
     if event.inaxes==ax1: ax = "real"
     elif event.inaxes==ax2: ax = "imag"
     else: return
-    
+
     key = event.key
     if key=="1":
         lines[ax].draw_swipe(int(event.xdata), event.ydata)
@@ -211,18 +142,18 @@ def bezier_smooth(vertices_dict):
     return(X_out, Y_out)
 
 def lengthen_samples(samples, loops):
-    """Concatenate multiple samples to make the resulting sound audible."""
+    """Stream loop the ~0.1s sound to make it audible."""
     s_out = array("h", [])
     for i in range(loops):
         s_out = s_out + samples
     return(s_out)
 
 def convert_to_sound(samples_fft, fft_length):
-    """Convert scaled data to sound."""
-    def plot_samples(start, end):
+    """Convert FFT data to sound."""
+    def plot_samples(end):
         fig = plt.figure(2, figsize=(8.0,5.4))
-        Y = samples[start:end]
-        X = array("h", [i for i in range(len(Y))])
+        Y = samples[:end]
+        X = np.arange(end, dtype=int)
         plt.plot(X, Y, "r+")
         plt.show()
 
@@ -233,35 +164,79 @@ def convert_to_sound(samples_fft, fft_length):
             
     check(samples_fft)
     samples_ifft = np.fft.ifft(samples_fft)
-    samples = np.array([], dtype=np.int32)
     samples = array("h", [int(i.real) for i in samples_ifft])
-    samples = lengthen_samples(samples, 88200//fft_length)
+
     #extend short samples (~0.1s) to make audible (~2s)
-    sound = AudioSegment.silent(1, frame_rate=44100)
+    samples = lengthen_samples(samples, 88200//fft_length)
+    sound = AudioSegment.silent(1, frame_rate=rate)
     sound = sound._spawn(samples)
-    if "n" in effects:
+    if params["norm"]==1 or params["norm"]=="True":
         sound = normalize(sound)
-    elif "f" in effects:
+    if params["fade"]==1 or params["fade"]=="True":
         sound = sound.fade_out(len(sound))
     print("Playing... ", end="")
     play(sound)
     print("Done, length of sound is", len(sound),
           "Loudness is", round(sound.max_dBFS,2))
-    if plot_domain!=0:
-        plot_samples(0, int(plot_domain))
-    if out_path!="None" and out_path: #not None or "None"
-        sound.export(out_path, format="wav")
+    if "plot" in params:
+        plot_samples(int(params["plot"]))
+    if "y" in params:
+        sound.export(params["y"], format="wav")
 
-lim_x, fft_length, effects, plot_domain, out_path = input_params()
-swipe_w = get_swipe_width(lim_x)
+def get_sound_samples(fp, start=0, window=True):
+    """Get mono sound. Start is measured in ms."""
+    sound_orig = AudioSegment.from_wav(fp)
+    sound_tracks = sound_orig.split_to_mono()
+    #print("length of sound is", len(sound))
+    samples = sound_tracks[0].get_array_of_samples()
+    samples = samples[int(ms2sample(start)):int(ms2sample(start))+fft_length]
+    
+    if window:
+        window_func = np.hanning(len(samples))
+        samples *= window_func
+
+    return(samples)
+
+params = input_and_parse_params()
+print("Parameters are", params)
+lim_x = params["x"]
+fft_length = params["l"]
+rate = 44100 #Other sample rates may generate noise.
+x_scale = rate / fft_length
+y_scale = 60000 / lim_x     #prevent overflow
 plt.ion()
 fig, (ax1,ax2) = plt.subplots(2, 1, sharex=True, figsize=(4.2,5.6))
-ax1.set(xlim=(0,lim_x), ylim=(0,10000), title="Real part")
-ax2.set(xlim=(0,lim_x), ylim=(0,10000), title="Imaginary part")
+ax1.grid(True, "major", "x")
+ax2.grid(True, "major", "x")
 
-line_imag, = ax2.plot([], [], color="C1")
-Real = LineSegment(ax1)
-Imag = LineSegment(ax2)
+def conversion_to(x):
+    return(x*x_scale)
+def conversion_from(x):
+    return(x/x_scale)
+ax_axis = ax2.secondary_xaxis(-0.1, functions=(conversion_to, conversion_from))
+ax_axis.set_xlabel("frequency [Hz]")
+ax1.set(xlabel=None, xlim=(0,lim_x), title="Real part")
+ax2.set(xlabel=None, xlim=(0,lim_x), title="Imaginary part")
+
+if params["st"]=="fromfile" or params["st"]=="f":
+    in_samples = get_sound_samples(params["i"], params["ss"], params["window"])
+    in_fft = np.fft.fft(in_samples)[:lim_x]
+    #print("len fft is ", len(in_fft))
+    in_reals = in_fft.real / y_scale
+    in_imags = in_fft.imag / y_scale
+
+    y_max = max(max(abs(in_reals)), max(abs(in_imags)))
+    ax1.set(ylim=(-y_max, y_max))
+    ax2.set(ylim=(-y_max, y_max))
+
+    Real = LineSegment(ax1, in_reals)
+    Imag = LineSegment(ax2, in_imags)
+else:
+    Real = LineSegment(ax1)
+    Imag = LineSegment(ax2)
+    ax1.set_ylim(-5000,5000)
+    ax2.set_ylim(-5000,5000)
+
 lines = {"real":Real, "imag":Imag}
 fig.canvas.mpl_connect("key_press_event", detect_keyboard)
 fig.canvas.mpl_connect("motion_notify_event", detect_swipe)
